1a8ffc0726facb47437de8c65daf08c616b9602d
Vijitha Bro <vijithaisuru@gmail.com>
2018-06-11 16:07:00 +0530

    Completed Decoder

--- SuperAHB.srcs/sources_1/new/Decoder.v
+++ SuperAHB.srcs/sources_1/new/Decoder.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
+// Company: University of Moratuwa
 // Engineer: 
 // 
 // Create Date: 06/11/2018 03:07:19 PM
@@ -13,16 +13,49 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision: 
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
+// Reference: https://www.researchgate.net/figure/An-example-AHB-system-with-one-master-and-one-slave_fig5_221004353
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module Decoder(
-    input [12:0] HADDR,
-    output HSELx1,
-    output HSELx2
-    );
+    input [11:0] HADDR,
+    input HCLK,
+    input HRESETn,
+    output reg HSELx1,
+    output reg HSELx2,
+    output reg HSELx3
+    );    
+    
+    always @ (posedge HCLK) 
+        case (HADDR)
+            12'b1111_0000_0000:
+                begin
+                    HSELx1 = 1'b1;
+                    HSELx2 = 1'b0;
+                    HSELx3 = 1'b0;
+                end
+            12'b0000_1111_0000:
+                begin
+                    HSELx1 = 1'b0;
+                    HSELx2 = 1'b1;
+                    HSELx3 = 1'b0;
+                end
+            12'b0000_0000_1111:
+                begin
+                    HSELx1 = 1'b0;
+                    HSELx2 = 1'b0;
+                    HSELx3 = 1'b1;
+                end
+            default:
+                begin
+                    HSELx1 = 1'b0;
+                    HSELx2 = 1'b0;
+                    HSELx3 = 1'b0;
+                end
+        endcase 
+    
 endmodule
