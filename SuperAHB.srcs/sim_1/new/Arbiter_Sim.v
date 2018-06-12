e06df03f2d76dccf6b0a6f22aacc42e2b886d06f
Vijitha Bro <vijithaisuru@gmail.com>
2018-06-12 18:17:45 +0530

    Added testbench for Arbiter

---
+++ SuperAHB.srcs/sim_1/new/Arbiter_Sim.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/12/2018 06:16:37 PM
+// Design Name: 
+// Module Name: Arbiter_Sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Arbiter_Sim(
+
+    );
+endmodule
